837e9e00cdbbe3fe5200a6dcffd46ed507b72efe
AaronWong <aaronwonghan@gmail.com>
2023-02-07 03:32:34 -0800

    Initial commit

---
+++ src/AvalonEdit.Antlr.Core/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace AvalonEdit.Antlr.Core
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ tests/AvalonEdit.Antlr.Core.Tests/AvalonEdit.Antlr.Core.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace AvalonEdit.Antlr.Core.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
